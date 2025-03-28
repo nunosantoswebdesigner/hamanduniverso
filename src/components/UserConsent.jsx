a26322388573d2b55b4e6f4783168ce4db3a8108
Nuno Santos <42045537+nunosantoswebdesigner@users.noreply.github.com>
2025-03-28 10:21:07 +0000

    Add files via upload

--- src/components/UserConsent.jsx
+++ src/components/UserConsent.jsx
@@ -1,14 +1,12 @@
 import { useState, useEffect } from "react";
 import Cookies from "js-cookie";
 import { useLangContext } from '../contexts/LangContext';
-import { background } from "../assets";
 
 export default function UserConsent() {
   const { t } = useLangContext();
   const [showPopup, setShowPopup] = useState(false);
 
   useEffect(() => {
-    // Verifica se o cookie de consentimento já foi definido
     if (!Cookies.get("UserConsent")) {
       setShowPopup(true);
     }
@@ -22,11 +20,11 @@ export default function UserConsent() {
   if (!showPopup) return null;
 
   return (
-    <div className="fixed bottom-5 right-5 bg-gray-900 text-white p-4 rounded-md flex items-center gap-4 z-50 w-[400px]">
+    <div className="fixed bottom-0 right-0 bg-gray-900 text-white p-4 rounded-md flex items-center z-50">
       <p>{t('cocckies_texto')}.</p>
       <button
         onClick={acceptCookies}
-        className="text-black px-4 py-2 rounded-md font-bold"
+        className="text-black px-4 py-2 rounded-md font-bold ml-4"
         style={{backgroundColor: '#f9b090'}}
       >
         {t('aceitar')}
